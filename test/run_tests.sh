693a135b274accf48c3fd249767f41ca6a4c6e7f
bo1929 <asapci@ucsd.edu>
2024-05-06 21:59:05 +0000

    Minor bug fix in test script.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -11,7 +11,8 @@ sleep 3
 
 if [ ! -d taxonomy ]; then
   mkdir -p taxonomy
-  wget https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz && tar -xvf taxdump.tar.gz -C taxonomy rm -f taxdump.tar.gz
+  wget https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
+  tar -xvf taxdump.tar.gz -C taxonomy  && rm -f taxdump.tar.gz
 fi
 
 echo "Please enter:"
